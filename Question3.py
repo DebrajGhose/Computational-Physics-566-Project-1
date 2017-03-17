e5a5553c6deca108fbb1cab9ece87964a5dd31f4
DebrajGhose <dg.chaos06@gmail.com>
2017-03-17 17:33:53 -0400

    Created function to generate particle on ring = 100 units

--- Question3.py
+++ Question3.py
@@ -6,6 +6,21 @@ from pylab import *
 
 #functions
 
+#generate new particle
+
+def generate_particle(size):
+    
+    posx = randint(1, size) - size/2; posy = randint(1, size) - size/2; #x and y positions of the particle
+
+    mag = np.linalg.norm([posx,posy]) #magnitude from origin of domain
+    
+    k = 100/mag #constant to make the points fall on a circle of radius 100
+    
+    posx = k*posx + size/2; posy = k*posy + size/2; #now particle is on circle of radius 100 pixels
+
+    return posx, posy
+    
+
 #check adjacency to cluster
 
 #assign new postiion to diffusing particle
@@ -21,12 +36,23 @@ from pylab import *
 #set up domain
 
 size = 200 #size of domain
-domain = numpy.zeros((size,size)) #this where all the magic happens
-cluster = numpy.zeros((size,size)) #store where cluster locations are
+
+domain = zeros((size,size)) #0 is empty space and 1 is 'stuff'
+domain[size/2,size/2] = 1; #this going to be my seed
 
 
 
 
+#first, generate particle on a ring of radius 100 units
+
+
+posx, posy = generate_particle(size)
+
+
+for t in range(1,10): #replace with while loop later
+    
+    
+    
 
 
 
@@ -40,6 +66,36 @@ cluster = numpy.zeros((size,size)) #store where cluster locations are
 
 #3b
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##______________________________________________________________________________
+
+#Test that generating particle on ring is working
+
+"""
+for t in range(1,10): #replace with while loop later
+    
+    posx, posy = generate_particle(size)
+    
+    mag = np.linalg.norm([posx-size/2,posy - size/2]) #magnitude from origin of domain
+
+    print posx,posy,mag
+
+
+"""
 
 
 
