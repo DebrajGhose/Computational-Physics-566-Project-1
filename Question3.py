eeed661efc49a8530d027aa9ff1c0f74f8806d40
DebrajGhose <dg.chaos06@gmail.com>
2017-03-17 18:11:57 -0400

    Implemented cluster, and now LDA is working!

--- Question3.py
+++ Question3.py
@@ -18,11 +18,11 @@ def generate_particle(size):
     
     posx = k*posx + size/2; posy = k*posy + size/2; #now particle is on circle of radius 100 pixels
 
-    return posx, posy
+    return int(posx), int(posy)
     
 #do random walk
 
-def do_the_cha_cha(size,posx.posy):
+def do_the_cha_cha(size,posx,posy):
     
     step = randint(1,5) #choose one of four places for the patch to move to 
     
@@ -44,7 +44,7 @@ def do_the_cha_cha(size,posx.posy):
         
     #enforce periodic boundary conditions
     
-    posx = mod(posx-1,size) + 1; posy = mod(posy-1,size) + 1; #so if the particle goes over the edge, it will reneter from other side
+    posx = mod(posx,size); posy = mod(posy,size); #so if the particle goes over the edge, it will reneter from other side
     
     return posx, posy
         
@@ -52,10 +52,18 @@ def do_the_cha_cha(size,posx.posy):
     
 #check adjacency to cluster
 
-#assign new postiion to diffusing particle
+def check_cluster(posx,posy,size,domain):
+    
+    bx = mod(posx- 1,size); fx = mod(posx + 1,size); #pixels behind or in front of x and y positions of particle
+    by = mod(posy- 1,size) ; fy = mod(posy+ 1,size);
 
 
+    if domain[bx,posy] == 1 or domain[fx,posy] == 1 or domain[posx,by] == 1 or domain[posx,fy] == 1: #check if there is an adjacent cluster
+        
+        domain[posx,posy] = 1 #cluster is established
 
+    return domain
+    
 
 
 #3a
@@ -65,7 +73,7 @@ def do_the_cha_cha(size,posx.posy):
 #set up domain
 
 size = 200 #size of domain
-
+cluster_reach = 0 #cluster growth parameter, when this gets big, we terminate
 domain = zeros((size,size)) #0 is empty space and 1 is 'stuff'
 domain[size/2,size/2] = 1; #this going to be my seed
 
@@ -78,18 +86,23 @@ domain[size/2,size/2] = 1; #this going to be my seed
 posx, posy = generate_particle(size) #generate particle on a ring of radius 100
 
 
-for t in range(1,10): #replace with while loop later
     
     
     #check for cluster adjcency
     
+    domain = check_cluster(posx,posy,size,domain)
     
+    if domain[posx,posy] == 1: #generate new particle if spot is taken
+    
+        cluster_reach = np.linalg.norm([posx-size/2,posy-size/2]) #see how large cluster is
+
+        posx, posy = generate_particle(size) #generate particle on a ring of radius 100
 
     #do the random walk
     
     posx, posy = do_the_cha_cha(size,posx,posy) #NOTE: No need of particle decay since we enforced periodic boundary conditions
     
-
+plt.imshow(domain)
 
 
 
